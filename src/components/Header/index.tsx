8ec12798ac1d573e0267d3f4cc44dff871e3917c
José Leonardo Nascimento Garcia <leogarcia.net@hotmail.com>
2023-01-12 10:22:53 -0300

    chore: created component Header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,28 @@
+import { useNavigation } from '@react-navigation/native';
+
+import { BackButton, BackIcon, Container, Logo } from './styles';
+
+import LogoImg from '@assets/logo.png';
+
+type Props = {
+  showBackButton?: boolean
+}
+
+export function Header({ showBackButton = false }: Props) {
+  const navigation = useNavigation();
+
+  function handleGoBack() {
+    navigation.navigate('groups');
+  }
+
+  return (
+    <Container>
+      { showBackButton && 
+        <BackButton onPress={handleGoBack}>
+          <BackIcon />
+        </BackButton>
+      }
+      <Logo source={LogoImg}/>
+    </Container>
+  );
+}
