e1048063666bcbc44f2f11e46eb2aedecafe63f4
José Leonardo Nascimento Garcia <leogarcia.net@hotmail.com>
2023-01-12 10:24:28 -0300

    chore: config project and install dependencies

---
+++ App.tsx
@@ -0,0 +1,20 @@
+import { StatusBar } from "react-native";
+import { ThemeProvider } from "styled-components";
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from "@expo-google-fonts/roboto";
+
+import { Routes } from "./src/routes/index";
+
+import { Loading } from "@components/Loading";
+
+import theme from "./src/theme";
+
+export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
+  return (
+    <ThemeProvider theme={theme}>
+      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
+      { fontsLoaded ? <Routes /> : <Loading /> }
+    </ThemeProvider>
+  );
+}
